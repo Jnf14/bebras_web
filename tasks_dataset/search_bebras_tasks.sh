a97d124432c703183ae6342e494b892249911bb6
Jérémie Frei <36692804+Jnf14@users.noreply.github.com>
2023-03-27 20:27:27 +0200

    Update search_bebras_tasks.sh

--- tasks_dataset/search_bebras_tasks.sh
+++ tasks_dataset/search_bebras_tasks.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # Find all folders which contain file matching 20*.md and copy them to this folder
-find /path/to/task-dev -name "20*.md" -exec sh -c 'cp -Rp "${0%/*}" /path/to/this/folder' {} \;
+find /path/to/task-dev -name "20*.md" -exec sh -c 'cp -Rp "${0%/*}" .' {} \;
 
 # Delete all files with .odt extension 
-find /path/to/this/folder -name "*.odt" -type f -delete
+find . -name "*.odt" -type f -delete
 
 # Delete empty folders
 find . -type d -empty -delete
