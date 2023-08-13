a1b095c2a21bfb395afc55a9aee96231eec2af7c
jnf14 <jeremie.frei@epfl.ch>
2023-08-13 22:21:16 +0200

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,24 @@
-import prisma from './prisma'
-import { insertAllTasks, insertTask } from './task'
+import prisma from "../app/libs/prismadb";
+import { insertAllTasks, insertTask } from "./task";
 
 async function main() {
-    // Clear database table
-    // TODO
+  // Clear database table
+  // TODO
 
-    // Test for one task
-    // insertTask('2022-VN-05_Colorful_tower')
+  // Test for one task
+  // insertTask("2022-VN-05_Colorful_tower");
 
-    // Insert all existing tasks
-    insertAllTasks()
+  // Insert all existing tasks
+  insertAllTasks();
 }
 
 // Run main seeding function
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
