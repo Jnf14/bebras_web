34572c43dbf18978e67f7817f09a5f35c6870619
jnf14 <jeremie.frei@epfl.ch>
2023-08-14 00:57:12 +0200

    Update AgeFilter.tsx

--- app/components/filters/AgeFilter.tsx
+++ app/components/filters/AgeFilter.tsx
@@ -1,14 +1,11 @@
-// 'use client';
+"use client";
 
-// interface AgeFilterProps {
-//     ages: string[],
+interface AgeFilterProps {
+  ages: string[];
+}
 
-// }
+const AgeFilter = () => {
+  return <h1>Ages</h1>;
+};
 
-// const AgeFilter = () => {
-//   return (
-
-//   );
-// };
-
-// export default AgeFilter;
+export default AgeFilter;
