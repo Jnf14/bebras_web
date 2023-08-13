153334a5dd46ed4de33e4cad71732661e3960b6a
jnf14 <jeremie.frei@epfl.ch>
2023-08-13 22:19:42 +0200

    Delete HtmlFrame.tsx

--- app/[taskId]/components/HtmlFrame.tsx
+++
@@ -1,9 +0,0 @@
-// const HtmlFrame = () => {
-//     return (  
-//         <div className=' w-4/5'>
-//             <iframe srcDoc={htmlText} width="100%" height="100%"> </iframe>
-//         </div>
-//     );
-// }
- 
-// export default HtmlFrame;
