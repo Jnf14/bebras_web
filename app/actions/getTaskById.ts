67ee94d212689fcea396021f7eac3bff6831d2bd
jnf14 <jeremie.frei@epfl.ch>
2023-08-14 19:34:24 +0200

    Fix TaskPage props error

--- app/actions/getTaskById.ts
+++ app/actions/getTaskById.ts
@@ -1,16 +1,17 @@
 import prisma from "@/app/libs/prismadb";
 
-interface IParams {
-  taskId: string;
+export interface IParams {
+  taskId?: string;
 }
 /**
  * Get one task from Task collection given its id
  * @param taskId the id of the task (format YYYY-CC-XX-lll)
  * @returns the task object
  */
-export default async function getTaskById({ taskId }: IParams) {
+export default async function getTaskById(params: IParams) {
   try {
-    const task = await prisma.task.findUnique({
+    const { taskId } = params;
+    const task = await prisma.task.findFirst({
       where: {
         taskId: taskId,
       },
@@ -22,7 +23,6 @@ export default async function getTaskById({ taskId }: IParams) {
 
     return {
       ...task,
-      // createdAt: listing.createdAt.toString(),
     };
   } catch (error: any) {
     throw new Error(error);
--- app/tasks/[taskId]/page.tsx
+++ app/tasks/[taskId]/page.tsx
@@ -1,15 +1,15 @@
 import TaskHtmlFrame from "../../components/tasks/TaskHtmlFrame";
 import { getTaskHtmlString } from "@/app/libs/utils";
-import getTaskById from "@/app/actions/getTaskById";
+import getTaskById, { IParams } from "@/app/actions/getTaskById";
 import Empty from "@/app/components/Empty";
+import TaskKeyword from "@/app/components/tasks/TaskKeyword";
 
 interface TaskPageProps {
-  taskId: string;
+  params: IParams;
 }
 
-export default async function TaskPage(props: TaskPageProps) {
-  const { taskId } = props;
-  const task = await getTaskById({ taskId });
+export default async function TaskPage({ params }: TaskPageProps) {
+  const task = await getTaskById(params);
 
   if (task == null) {
     return <Empty />;
@@ -18,9 +18,16 @@ export default async function TaskPage(props: TaskPageProps) {
   const htmlText = getTaskHtmlString(task.filePath);
 
   return (
-    <div>
-      <h1 className="text-lg font-bold">{task.engTitle}</h1>
-      <TaskHtmlFrame htmlText={htmlText} />
+    <div className="grid grid-cols-4">
+      <div className="col-span-1 px-1">
+        <h1 className="text-lg font-bold">Keywords</h1>
+        {task.bebras_keywords?.map((keyword) => (
+          <TaskKeyword keyword={keyword} />
+        ))}
+      </div>
+      <div className="col-span-3 ml-3">
+        <TaskHtmlFrame htmlText={htmlText} />
+      </div>
     </div>
   );
 }
