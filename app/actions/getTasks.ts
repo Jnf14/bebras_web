4bb21b751578354bdb04294d89116edf8ff387fb
Nolan Chappuis <nolan.chappuis@epfl.ch>
2023-08-21 21:35:10 +0200

    Add categories filter for task selections

--- app/actions/getTasks.ts
+++ app/actions/getTasks.ts
@@ -4,25 +4,28 @@ import { isArray } from "bebras/out/util";
 
 export interface ISearchParams {
   ageCategories: string[];
+  categories: string[];
 }
 
 /**
  * Get tasks from data file based on given filters
  */
 export default function getTasks(params: ISearchParams): Task[] {
-  const { ageCategories } = params;
+  const { ageCategories, categories } = params;
 
   console.log(ageCategories);
+  console.log(categories);
 
   let tasks: Task[];
+  tasks = data as Task[]
   if (ageCategories) {
     if (Array.isArray(ageCategories)) {
-      tasks = data.filter((task) => {
+      tasks = tasks.filter((task) => {
         const taskAges = task.ageCategories.map((c) => c.name);
         return ageCategories.every((c) => taskAges.includes(c));
       });
     } else {
-      tasks = data.filter((task) => {
+      tasks = tasks.filter((task) => {
         const taskAges = task.ageCategories.map((c) => c.name);
         return new Array(ageCategories).every((c) => taskAges.includes(c));
       });
@@ -34,7 +37,20 @@ export default function getTasks(params: ISearchParams): Task[] {
     //   return taskAgeCategories.every((cat) => ageCategories.includes(cat));
     // });
   } else {
-    tasks = data as Task[];
+    //tasks = data as Task[];
+  }
+  if (categories){
+    if (Array.isArray(categories)) {
+      tasks = tasks.filter((task) => {
+        const taskCategories = task.bebrasCategories.map((c) => c.category);
+        return categories.every((c) => taskCategories.includes(c));
+      });
+    } else {
+      tasks = tasks.filter((task) => {
+        const taskCategories = task.bebrasCategories.map((c) => c.category);
+        return new Array(categories).every((c) => taskCategories.includes(c));
+      });
+    }
   }
 
   console.log(tasks.length);
