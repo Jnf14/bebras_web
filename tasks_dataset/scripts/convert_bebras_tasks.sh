6c8ef6fd9e54188cdfe8335984902e372f430d95
jnf14 <jeremie.frei@epfl.ch>
2023-08-26 17:15:59 +0200

    Update convert_bebras_tasks.sh

--- tasks_dataset/scripts/convert_bebras_tasks.sh
+++ tasks_dataset/scripts/convert_bebras_tasks.sh
@@ -1,8 +1,5 @@
-#! /bin/bash
-
-# Converts bebras tasks to html (without graphics !!)
-#find . -name "20*.md" -execdir sh -c 'bebras convert html {} --output-file ../html_tasks/"${0%.*}".html' {} \;
-
-
 # Converts bebras tasks to pdf
-find . -name "20*.md" -execdir sh -c 'bebras convert pdf {} --output-file ../pdf_tasks/"${0%.*}".pdf' {} \;
+find . -name "20*-fra*.md" -execdir sh -c 'bebras convert pdf {} --output-file ../../public/tasks/pdfFiles/"${0%.*}".pdf' {} \;
+
+# Delete all files with .json extension 
+find . -name "20*.pdfmeta.json" -type f -delete
