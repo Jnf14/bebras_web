4f717ebb55626e51e598ac24d4c84d27b3182c8e
jnf14 <jeremie.frei@epfl.ch>
2023-08-31 22:42:44 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,15 @@
 export default async function HomePage() {
   return (
-    <div className="flex justify-center my-20">
-      <div>
-        <h1 className=" font-bold text-lg p-3">
+    <div className="flex justify-center my-20 mx-60">
+      <div className="flex flex-col items-center gap-14">
+        <h1 className=" font-bold text-lg">
           Bienvenue sur la nouvelle plateforme Bebras !
         </h1>
 
         <span className=" text-wrap">
-          Son objectif est de répertorier les tâches des anciens concours du
-          Castor Informatique dans un but pédagogique.
+          Son objectif est de répertorier les tâches des anciens concours Castor
+          Informatique et d'offrir la possibilité de les rechercher par thème,
+          âge ou mot-clé.
         </span>
       </div>
     </div>
